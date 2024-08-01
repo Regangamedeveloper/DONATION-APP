174a18634295aeb72107c2643277d8a719b72a22
Regangamedeveloper <bagumaregan@icloud.com>
2024-08-01 18:55:40 +0300

    #yo sup

--- src/charity/components/navbar/Navbar.jsx
+++ src/charity/components/navbar/Navbar.jsx
@@ -4,35 +4,8 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import Diversity1SharpIcon from "@mui/icons-material/Diversity1Sharp";
-
-// LanguageIcon component
-const LanguageIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      className="icon"
-    >
-      <path d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17c-.53 2.57-2.05 4.78-3.95 6.8l.03.03l2.53 2.51C14.63 14.08 15.36 13 17 13v-2c-1.96 0-3.5 1.56-3.95 3.53z" />
-    </svg>
-  );
-};
-
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState("English");
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleLanguageChange = (language) => {
-    setSelectedLanguage(language);
-    setIsDropdownOpen(false);
-  };
-
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -40,11 +13,10 @@ const Navbar = () => {
           <Diversity1SharpIcon />
         </div>
         <div className="items">
-          <div className="item" >
-            <LanguageIcon />
-          
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
           </div>
-
           <div className="item">
             <DarkModeOutlinedIcon className="icon" />
           </div>
@@ -63,7 +35,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
