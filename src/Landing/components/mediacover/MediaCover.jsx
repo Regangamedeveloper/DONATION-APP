b864a0807ee76d381a4dae40c512d554a440c24c
mukisapaulk <mukisapaul480@gmail.com>
2024-10-06 21:54:49 +0300

    fixed some landing bugs

--- src/Landing/components/mediacover/MediaCover.jsx
+++ src/Landing/components/mediacover/MediaCover.jsx
@@ -13,6 +13,7 @@ const MediaCover = () => {
         <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.compassion.com%2Fmultimedia%2Fmake-a-donation-children-in-togo.jpg&f=1&nofb=1&ipt=8190a21f6f92780c2bc40b67dec3b332cb125e1b27cf6eed01e464a27afa2419&ipo=images" alt="image on trial" />
         <div className="featured-intel">
         <h1>help the needy</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel architecto consequuntur recusandae harum hic iste ipsa perferendis aliquam, iusto temporibus!</p>
         <button>Donate Now</button>
         </div>
         </div>
@@ -20,6 +21,7 @@ const MediaCover = () => {
         <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.compassion.com%2Fmultimedia%2Fmake-a-donation-children-in-togo.jpg&f=1&nofb=1&ipt=8190a21f6f92780c2bc40b67dec3b332cb125e1b27cf6eed01e464a27afa2419&ipo=images" alt="image on trial" />
         <div className="featured-intel">
         <h1>help the needy</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel architecto consequuntur recusandae harum hic iste ipsa perferendis aliquam, iusto temporibus!</p>
         <button>Donate Now</button>
         </div>
         </div>
@@ -27,6 +29,7 @@ const MediaCover = () => {
         <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.compassion.com%2Fmultimedia%2Fmake-a-donation-children-in-togo.jpg&f=1&nofb=1&ipt=8190a21f6f92780c2bc40b67dec3b332cb125e1b27cf6eed01e464a27afa2419&ipo=images" alt="image on trial" />
         <div className="featured-intel">
         <h1>help the needy</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel architecto consequuntur recusandae harum hic iste ipsa perferendis aliquam, iusto temporibus!</p>
         <button>Donate Now</button>
         </div>
         </div>
